db1bc89d7a2df8871ce1b4c1720a1e6ef7c3b4e6
Corbin Sykes <corbin.sykes@gmail.comm>
2013-06-25 11:23:18 -0400

    added add function

--- main.rb
+++ main.rb
@@ -4,3 +4,30 @@ require 'sinatra/reloader' if development?
 get '/' do
   "Hello!"
 end
+
+get '/hello_world' do
+  "Hello, World!"
+end
+
+get '/hello_me' do
+  @me = "Corbin"
+  return "Hello, #{@me}!"
+end
+
+get '/hello_you/:name' do
+  @name = params[:name]
+  return "Hello, #{@name}!"
+end
+
+get '/names/:first/:last' do
+  @first = params[:first]
+  @last = params[:last]
+  return "Get money, get paid, #{@first.capitalize} #{@last.capitalize}."
+end
+
+get '/calc/add/:first/:second' do
+  @first = params[:first].to_i
+  @second = params[:second].to_i
+  (@first + @second).to_s
+
+end
